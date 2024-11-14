69dbb1384da3e99858496259034bd47694e19376
Kyle Wiskel <kwiskel@gmail.com>
2024-11-14 15:06:54 -0700

    Fix formatting

--- app/ui/NavigationBar.tsx
+++ app/ui/NavigationBar.tsx
@@ -7,33 +7,35 @@ import Link from 'next/link';
 
 export default function NavigationBar() {
   const theme = useTheme();
-  const { isDarkMode, changeMode } = useContext(ThemeContext); // Access the theme state and function
+  const { changeMode } = useContext(ThemeContext); // Access the theme state and function
 
   return (
     <Toolbar>
-      <Box sx={{ display: 'flex', justifyContent: 'center', gap: '5%', width: '100%' }}>
-        <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
-          Projects
-        </Button>
-        <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
-          Work Experience
-        </Button>
-        <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
-          Contact
-        </Button>
-       <Button
-          component={Link}
-          href="https://kylewiskel.s3.us-east-2.amazonaws.com/KyleWiskel_Resume.pdf"
-          rel="noopener noreferrer"
-          target="_blank"
-          sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}
-        >
-          Resume
-        </Button>
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: '5%', width: '100%', border: '1px solid' }}>
+        <Box>
+          <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
+            Projects
+          </Button>
+          <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
+            Work Experience
+          </Button>
+          <Button sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}>
+            Contact
+          </Button>
+          <Button
+            component={Link}
+            href='https://kylewiskel.s3.us-east-2.amazonaws.com/KyleWiskel_Resume.pdf'
+            rel='noopener noreferrer'
+            target='_blank'
+            sx={{ color: 'text.primary', '&:hover': { color: 'primary.main', background: 'inherit' } }}
+          >
+            Resume
+          </Button>
+        </Box>
+        <IconButton onClick={() => changeMode()}>
+          {theme.palette.mode === 'dark' ? <LightMode /> : <DarkMode />}
+        </IconButton>
       </Box>
-      <IconButton onClick={() => changeMode()}>
-        {theme.palette.mode === 'dark' ? <LightMode /> : <DarkMode />}
-      </IconButton>
     </Toolbar>
   );
 }
