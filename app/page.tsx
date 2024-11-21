ba659108810bb6071dcaca2d8d91d8a6823ea619
Kyle Wiskel <kwiskel@gmail.com>
2024-11-21 15:07:58 -0700

    Show more is only shown on desktop and once used you can show less

--- app/page.tsx
+++ app/page.tsx
@@ -15,46 +15,50 @@ export default function Page() {
   const workRef = useRef(null);
   const contactRef = useRef(null);
 
-  // const [showMore, setShowMore] = useState(false); // Controls expanded content
+  const [showMore, setShowMore] = useState(false); // Controls expanded content
 
-  // const setCollapse = () => {
-  //   if (showMore) window.scrollTo({ top: 0, behavior: 'smooth' });
-  //   setShowMore(!showMore);
-  // };
+  const setCollapse = () => {
+    if (!showMore) {
+      setShowMore(true);
+    }
+  };
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Box
-        sx={{
-          maxWidth: '1300px',
-          margin: {
-            xs: '25px 15px 50px 15px',
-            lg: '25px 100px 50px 100px',
-          },
-        }}
-      >
-        <NavigationBar projectRef={projectRef} workRef={workRef} contactRef={contactRef} />
-        <TitleSection />
-        {/* <ExpandMore
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box
           sx={{
-            display: 'block',
-            margin: 'auto',
-            fontSize: '50px',
-            color: theme.palette.text.primary,
-            cursor: 'pointer',
-            marginTop: '100px',
-            rotate: showMore ? '180deg' : '0deg',
+            maxWidth: '1300px',
+            margin: {
+              xs: '25px 15px 50px 15px',
+              lg: '25px 100px 50px 100px',
+            },
           }}
-          onClick={() => setCollapse()}
-        /> */}
-        <Collapse in={true} timeout={{ enter: 2000, exit: 0 }} sx={{ maxWidth: '100%' }}>
-          <>
-            <ProjectsSection ref={projectRef} />
-            <WorkSection ref={workRef} />
-            <ContactSection ref={contactRef} />
-          </>
-        </Collapse>
+        >
+          <NavigationBar projectRef={projectRef} workRef={workRef} contactRef={contactRef} />
+          <TitleSection />
+          <ExpandMore
+            sx={{
+              display: { xs: 'none', md: 'block' },
+              margin: 'auto',
+              fontSize: '50px',
+              color: theme.palette.text.primary,
+              cursor: 'pointer',
+              marginTop: '100px',
+              rotate: showMore ? '180deg' : '0deg',
+              visibility: showMore ? 'hidden' : 'visible',
+            }}
+            onClick={() => setCollapse()}
+          />
+          <Collapse in={showMore} timeout={{ enter: 2000, exit: 0 }} sx={{ maxWidth: '100%' }}>
+            <>
+              <ProjectsSection ref={projectRef} />
+              <WorkSection ref={workRef} />
+              <ContactSection ref={contactRef} />
+            </>
+          </Collapse>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
